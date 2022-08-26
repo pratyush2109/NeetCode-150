27a5f404ae54441aae9dbcc396fdaef1784d42dd
Pratyush Ambastha <39798918+pratyush2109@users.noreply.github.com>
2022-08-26 23:38:35 +0530

    Time: 3428 ms (36.68%), Space: 59.2 MB (72.06%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -6,7 +6,7 @@ class Solution:
             if t[0]>target[0] or t[1]>target[1] or t[2]>target[2]:
                 continue
                 
-            for i,v in enumerate(t):
+            for i in range(3):
                 if t[i]==target[i]:
                     good.add(i)
                     
