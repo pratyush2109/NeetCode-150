e7813d500564cb8bcaf089185ffd07b89a8445c1
Pratyush Ambastha <39798918+pratyush2109@users.noreply.github.com>
2022-08-29 15:10:48 +0530

    Time: 87 ms (92.84%), Space: 20.7 MB (79.27%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,16 +1,22 @@
 class Solution:
-    def countOne(self, m):
-        res=0
-        while m:
-            m&=(m-1)
-            res+=1
-        return res
-
     def countBits(self, n: int) -> List[int]:
-        ans=[]
-        i=0
+        if n==0:
+            return [0]
+        if n==1:
+            return [0,1]
+        if n==2:
+            return [0,1,1]
+        dp=[0]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        dp[2]=1
+        
+        i=3
         while i<=n:
-            ans.append(self.countOne(i))
+            if i%2==0:
+                dp[i]=dp[i//2]
+            else:
+                dp[i]=dp[i//2]+1
             i+=1
-            
-        return ans
+                
+        return dp
