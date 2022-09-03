abb8876b115d4de41d9d913c710ff0802cf36cdf
Pratyush Ambastha <39798918+pratyush2109@users.noreply.github.com>
2022-09-03 22:44:29 +0530

    Time: 97 ms (29.04%), Space: 14 MB (47.69%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,30 +1,29 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
+        fresh, time = 0, 0
         queue=[]
-        time, fresh=0, 0
         
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c]==1:
                     fresh+=1
-                if grid[r][c]==2:
+                elif grid[r][c]==2:
                     queue.append((r,c))
                 
-        directions=[[0,1],[0,-1],[1,0],[-1,0]]
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
         while queue and fresh>0:
             for i in range(len(queue)):
-                r, c = queue.pop(0)
+                r, c =queue.pop(0)
                 for dr,dc in directions:
-                    row, col=r+dr, c+dc
+                    row, col=r+dr,c+dc
                     if row not in range(rows) or col not in range(cols) or grid[row][col]!=1:
                         continue
                     grid[row][col]=2
                     queue.append((row,col))
                     fresh-=1
             time+=1
-            
+
         if fresh==0:
             return time
-        else:
-            return -1
+        return -1
