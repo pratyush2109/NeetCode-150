8d4f449b5e47004b596f24739ff6cb66a8d2671a
Pratyush Ambastha <39798918+pratyush2109@users.noreply.github.com>
2022-09-05 11:33:47 +0530

    Time: 700 ms (12.51%), Space: 21.8 MB (25.46%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,30 +3,29 @@ class Solution:
         if len(grid)==0:
             return 0
         
-        rows, cols = len(grid), len(grid[0])
-        visited=set()
+        rows,cols=len(grid),len(grid[0])
         islands=0
+        visited=set()
         
         def bfs(r,c):
-            queue=list()
+            queue=[]
             visited.add((r,c))
             queue.append((r,c))
             
             while queue:
-                row, col = queue.pop(0)
-                directions = [[1,0], [-1,0], [0,1], [0,-1]]
+                row,col=queue.pop(0)
+                directions=[[0,1],[0,-1],[1,0],[-1,0]]
                 
-                for dr, dc in directions:
-                    r, c = row+dr, col+dc
-                    if r in range(rows) and c in range(cols) and grid[r][c]=="1" and (r, c) not in visited:
+                for dr,dc in directions:
+                    r,c=row+dr,col+dc
+                    if r in range(rows) and c in range(cols) and (r,c) not in visited and grid[r][c]=="1":
                         queue.append((r,c))
                         visited.add((r,c))
-        
-        
+                        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]=="1" and (r,c) not in visited:
+                if (r,c) not in visited and grid[r][c]=="1":
                     bfs(r,c)
                     islands+=1
-        
+                    
         return islands
